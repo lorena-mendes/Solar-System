21e7af6a704509d1cfed1c526cc554fa9e6f9223
lorena-mendes <lorena.lccma@gmail.com>
2023-01-06 14:26:14 -0300

    Finalizei css

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -5,12 +5,14 @@ class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="name">{ name }</p>
-        <p data-testid="year">{ year }</p>
-        <p data-testid="country">{ country }</p>
-        <p data-testid="destination">{ destination }</p>
-      </div>
+      <section className="section-missions">
+        <div data-testid="mission-card" className="mission">
+          <p data-testid="name">{ name }</p>
+          <p data-testid="year">{ year }</p>
+          <p data-testid="country">{ country }</p>
+          <p data-testid="destination">{ destination }</p>
+        </div>
+      </section>
     );
   }
 }
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -6,16 +6,18 @@ import MissionCard from './MissionCard';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div>
         <Title headline="Missões" />
-        {missions.map(({ name, year, country, destination }) => (
-          <MissionCard
-            name={ name }
-            year={ year }
-            country={ country }
-            destination={ destination }
-            key={ name }
-          />))}
+        <div data-testid="missions" className="missions">
+          {missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+              key={ name }
+            />))}
+        </div>
       </div>
     );
   }
